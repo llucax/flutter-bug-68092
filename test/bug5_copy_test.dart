7f3d9748b88453320b430d07cdb63ea39d3cb569
Leandro Lucarella <luca@llucax.com>
2020-11-02 12:58:31 +0100

    Change tests to have a test so they succeed
    If this is not done, flutter test exits with error because no tests were
    ran.

--- test/bug5_copy_test.dart
+++ test/bug5_copy_test.dart
@@ -1,6 +1,9 @@
 import 'package:bug5/bug5.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  int f({int x}) => null;
-  Registry().register(f);
+  test("test", () {
+    int f({int x}) => null;
+    Registry().register(f);
+  });
 }
